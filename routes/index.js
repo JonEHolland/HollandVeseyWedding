ec3c0dc2fb58071230bfbd053a750ca84491b229
Jonathan Holland <joholland@expedia.com>
2015-04-02 20:39:19 -0700

    scaffolded out db backed rsvp form

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
+
 var express = require('express');
+var pg = require('pg');
 var router = express.Router();
 
 // Get HomePage
@@ -8,7 +10,23 @@ router.get('/', function(req, res, next) {
 
 // GET RSVP Page
 router.get('/rsvp', function(req, res, next) {
-  res.render('rsvp');
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    	client.query('SELECT * FROM rsvp', function(err, result) {
+    		done();
+    		if (err) {
+    			console.error(err);
+    		}
+	      	res.render('rsvp', { results: result.rows });
+	    });
+ 	});
+});
+
+router.post('/rsvp', function(req, res, next) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    	client.query('insert into rsvp values ($1,$2)',[req.body.guestName, req.body.guestCount]);
+    	done();
+    	res.redirect('/');
+ 	});
 });
 
 // GET Directions Page
