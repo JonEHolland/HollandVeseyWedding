1210d82b1b7a2f6f859dacc051ade27a86395e8d
Jonathan Holland <joholland@expedia.com>
2015-04-06 14:16:02 -0700

    Added macys proxy hack to retrieve registry data

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,38 @@
 var express = require('express');
 var pg = require('pg');
-
+var http = require('follow-redirects').http;
+var cheerio = require('cheerio');
 var router = express.Router();
 
 // Get HomePage
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+// Macys API Proxy Hack
+router.get('/macys', function(req, res, next) {
+    getPage({
+        host: 'www.theknot.com',
+        port: 80,
+        method: 'GET',
+        path : '/us/amy-vesey-and-jonathan-holland-jun-2015/registry/content'
+    }, function(statusCode, response) {
+        
+        var registryItems = [];
+        var $ = cheerio.load(response);
+        var items = $('.registry-item');
+
+        for (var i = 0; i < items.length; i++) {
+            registryItems.push({
+                title : $('p', items[i]).text(),
+                price : $('.registry-price', items[i]).text(),
+                quantity : $('.registry-quantity', items[i]).text().replace('Wants','Would Love').replace('Needs','Still Needs'),
+                imageUrl : unescape($('.registry-image', items[i]).attr('style').replace('background-image: url(https://qa-beta.theknot.com/registry/proxy?url=','').slice(0, - 1))
+            });
+        }
+
+        res.json(registryItems);
+    });
 });
 
 // GET RSVP Report Page
@@ -46,5 +73,29 @@ router.post('/rsvp', function(req, res, next) {
     	res.render('rsvpSubmit');
  	});
 });
+
+var getPage = function(options, onResult)
+{
+    var req = http.request(options, function(res)
+    {
+        var output = '';
+        console.log(options.host + ':' + res.statusCode);
+        res.setEncoding('utf8');
+
+        res.on('data', function (chunk) {
+            output += chunk;
+        });
+
+        res.on('end', function() {
+            onResult(res.statusCode, output);
+        });
+    });
+
+    req.on('error', function(err) {
+        console.log(err);
+    });
+
+    req.end();
+};
 
 module.exports = router;
